7a60576ee8defacef7016ce700c9996eccba7913
Raphael-dln <delannay.raphael@gmail.com>
2020-05-11 14:56:45 +0200

    email2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   after_create :welcome_send
 
-  def new
-    @user = User.new()
-  end
-  
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
